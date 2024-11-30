bc62f043e539f67e87abbe5cad339bdc15564169
Roko <rokoponjarac2@gmail.com>
2024-11-30 20:31:08 +0100

    add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import Image from "next/image";
+import Logo from "../../assets/icons/logo.svg";
 
 interface HeaderProps {}
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.header__container}></div>
+      <div className={styles.header__container}>
+        <div className={styles.header__logo}>
+          <Image src={Logo} alt="logo" />
+        </div>
+      </div>
     </header>
   );
 };
