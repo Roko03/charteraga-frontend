667cd7b330d7a729bba0ce64b89e6df6a25bc263
Roko <rokoponjarac2@gmail.com>
2024-11-30 11:24:15 +0100

    remove from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <h2>Ej</h2>
-    </div>
-  );
+  return <div className={styles.page}></div>;
 }
