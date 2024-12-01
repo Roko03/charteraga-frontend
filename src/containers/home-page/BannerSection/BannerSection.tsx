238391878a80e2d4cc282c4a03924e11d2e9869e
Roko <rokoponjarac2@gmail.com>
2024-12-01 16:44:41 +0100

    make banner component and change structure for home

---
+++ src/containers/home-page/BannerSection/BannerSection.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BannerSection.module.scss";
+
+const BannerSection: React.FC = () => {
+  return <section>BannerSection</section>;
+};
+
+export default BannerSection;
