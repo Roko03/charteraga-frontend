51ff20261b07676bb41df6211dc0d708b6359da3
Roko <rokoponjarac2@gmail.com>
2024-12-01 10:20:26 +0100

    make footer links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Footer from "@/components/Footer";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <Footer />
+    </div>
+  );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../assets/icons/logo.svg";
+import FooterLinks from "../FooterLinks";
 
 const Footer: React.FC = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <Link href={"/"}>
+          <Image src={Logo} alt="logo" />
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
