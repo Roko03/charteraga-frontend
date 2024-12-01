0090b2fcf7df6ae61897f0a529bdce6346c53e15
Roko <rokoponjarac2@gmail.com>
2024-12-01 17:19:01 +0100

    make banner style

--- src/containers/home-page/BannerSection/BannerSection.tsx
+++ src/containers/home-page/BannerSection/BannerSection.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "./BannerSection.module.scss";
-import video from "../../../assets/video/banner-video.mp4";
 
 const BannerSection: React.FC = () => {
   return (
     <section className={styles.banner_section}>
-      <video src={video} autoPlay muted loop />
+      <div className={styles.banner}>
+        <div className={styles.banner__header}></div>
+        <video
+          src={require("../../../assets/video/banner-video.mp4")}
+          autoPlay
+          muted
+          loop
+          className={styles.banner__video}
+        />
+        <div className={styles.banner__footer}></div>
+      </div>
     </section>
   );
 };
