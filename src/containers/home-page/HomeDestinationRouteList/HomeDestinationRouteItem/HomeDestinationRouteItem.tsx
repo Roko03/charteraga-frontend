97e010e578c815667adb0d6327e8771f499e611d
Roko <rokoponjarac2@gmail.com>
2024-12-02 16:26:02 +0100

    style home destination route

--- src/containers/home-page/HomeDestinationRouteList/HomeDestinationRouteItem/HomeDestinationRouteItem.tsx
+++ src/containers/home-page/HomeDestinationRouteList/HomeDestinationRouteItem/HomeDestinationRouteItem.tsx
@@ -1,19 +1,54 @@
 import React from "react";
 import styles from "./HomeDestinationRouteItem.module.scss";
 import Link from "next/link";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import RangeComponent from "@/components/RangeComponent/RangeComponent";
-const HomeDestinationRouteItem: React.FC = () => {
+import ArrowRight from "../../../../assets/icons/arrow.svg";
+
+interface HomeDestinationRouteItemProps {
+  imageUrl: StaticImageData;
+  destinationTitle: string;
+  destinationPrice: number;
+  destinationPartyAbility?: number;
+  destinationAdventureAbility?: number;
+}
+
+const HomeDestinationRouteItem: React.FC<HomeDestinationRouteItemProps> = ({
+  imageUrl,
+  destinationTitle,
+  destinationPrice,
+  destinationPartyAbility = null,
+  destinationAdventureAbility = null,
+}) => {
   return (
-    <Link href={"/destination"} className={styles.home_destination_route_item}>
-      {/* <Image src={"/"} alt="destination-image" /> */}
-      <div className={styles.home_destination_route_item__detail}>
-        <div className={styles.home_destination_route_item__main}>
-          <p>From €710/person </p>
-          <h2>Party Route</h2>
+    <Link href={"/destination"} className={styles.home_destination_route}>
+      <Image
+        src={imageUrl}
+        alt="destination-image"
+        className={styles.home_destination_route__banner}
+      />
+      <div className={styles.home_destination_route__overlay}></div>
+      <div className={styles.home_destination_route__detail}>
+        <div className={styles.home_destination_route__main}>
+          <p>From €{destinationPrice}/person</p>
+          <span className={styles.home_destination_route__flex}>
+            <h2>{destinationTitle} Route</h2>
+            <Image src={ArrowRight} alt="arrow-right" />
+          </span>
         </div>
-        <div className={styles.home_destination_route_item__ability}>
-          <RangeComponent rangeNumber={30} />
+        <div className={styles.home_destination_route__ability}>
+          {destinationPartyAbility && (
+            <div className={styles.home_destination_route__ability__item}>
+              <p>Party</p>
+              <RangeComponent rangeNumber={destinationPartyAbility} />
+            </div>
+          )}
+          {destinationAdventureAbility && (
+            <div className={styles.home_destination_route__ability__item}>
+              <p>Adventure</p>
+              <RangeComponent rangeNumber={destinationAdventureAbility} />
+            </div>
+          )}
         </div>
       </div>
     </Link>
--- src/containers/home-page/HomeDestinationRouteList/HomeDestinationRouteList.tsx
+++ src/containers/home-page/HomeDestinationRouteList/HomeDestinationRouteList.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./HomeDestinationRouteList.module.scss";
 import HomeDestinationRouteItem from "./HomeDestinationRouteItem";
+import RouteImageOne from "../../../assets/images/party-route.png";
+import RouteImageTwo from "../../../assets/images/ultra-route.png";
+import RouteImageThree from "../../../assets/images/greece-route.png";
+import RouteImageFour from "../../../assets/images/adventure-route.png";
 
 const HomeDestinationRouteList: React.FC = () => {
   return (
     <div>
-      <HomeDestinationRouteItem />
+      <HomeDestinationRouteItem
+        imageUrl={RouteImageOne}
+        destinationTitle="Party"
+        destinationPrice={710}
+        destinationPartyAbility={75}
+        destinationAdventureAbility={40}
+      />
     </div>
   );
 };
