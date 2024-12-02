146c8a2dac70de6ae2b3d6c06a1e430c04b72390
Roko <rokoponjarac2@gmail.com>
2024-12-02 01:00:22 +0100

    add text

--- src/containers/home-page/ExperienceSection/ExperienceSection.tsx
+++ src/containers/home-page/ExperienceSection/ExperienceSection.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import styles from "./ExperienceSection.module.scss";
 import ExperienceRatingList from "../ExperienceRatingList";
+import TaglineBox from "@/components/TaglineBox";
 
 const ExperienceSection = () => {
   return (
     <section className={styles.experience_section}>
       <div className={styles.experience_section__container}>
         <ExperienceRatingList />
+        <TaglineBox title={"WELCOME"} />
+        <div className={styles.experience_section__text}>
+          <h2>What is Charterag experience</h2>
+          <p>
+            Join us for a week of sailing, unique adventures and exclusive
+            parties in stunning locations. Explore new places daily. Dive into
+            local culture. Dance the nights away. Leave with new friends and
+            lifelong memories.
+          </p>
+        </div>
       </div>
     </section>
   );
