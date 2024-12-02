682871ab39736f8343b9589e47e044ad71d54611
Roko <rokoponjarac2@gmail.com>
2024-12-03 00:45:26 +0100

    make destination highlights section

--- src/containers/destination-page/Destination/Destination.tsx
+++ src/containers/destination-page/Destination/Destination.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DestinationBannerSection from "../DestinationBannerSection";
+import DestinationHighlightSection from "../DestinationHighlightSection/DestinationHighlightSection";
 
 const Destination = () => {
   return (
     <>
       <DestinationBannerSection />
+      <DestinationHighlightSection />
     </>
   );
 };
---
+++ src/containers/destination-page/DestinationHighlightSection/DestinationHighlightSection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./DestinationHighlightSection.module.scss";
+
+const DestinationHighlightSection = () => {
+  return (
+    <section className={styles.destination_highlight_section}>
+      DestinationHighlightSection
+    </section>
+  );
+};
+
+export default DestinationHighlightSection;
