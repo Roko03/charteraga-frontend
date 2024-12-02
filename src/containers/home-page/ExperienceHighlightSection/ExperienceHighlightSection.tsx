a6ab80bc707770006807139d02a820ea595f01d7
Roko <rokoponjarac2@gmail.com>
2024-12-02 17:25:06 +0100

    make experience highlight gallery

--- src/containers/home-page/ExperienceHighlightSection/ExperienceHighlightSection.tsx
+++ src/containers/home-page/ExperienceHighlightSection/ExperienceHighlightSection.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "./ExperienceHighlightSection.module.scss";
 import Title from "@/components/Title";
+import ExperienceHighlightGallery from "../ExperienceHighlightGallery";
 
 const ExperienceHighlightSection: React.FC = () => {
   return (
-    <section>
+    <section className={styles.experience_higlight_section}>
       <Title
         hasTagline={false}
         taglineVariant={"secondary"}
         title="Come live your best life"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam."
       />
+      <ExperienceHighlightGallery />
     </section>
   );
 };
