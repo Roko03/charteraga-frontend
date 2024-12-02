7c0754f5208eda6bd03a0e9c0ed020b7aa277935
Roko <rokoponjarac2@gmail.com>
2024-12-02 16:33:15 +0100

    style home destination route list

--- src/containers/home-page/HomeDestinationRouteList/HomeDestinationRouteList.tsx
+++ src/containers/home-page/HomeDestinationRouteList/HomeDestinationRouteList.tsx
@@ -8,13 +8,34 @@ import RouteImageFour from "../../../assets/images/adventure-route.png";
 
 const HomeDestinationRouteList: React.FC = () => {
   return (
-    <div>
+    <div className={styles.home_destination_route_list}>
       <HomeDestinationRouteItem
         imageUrl={RouteImageOne}
         destinationTitle="Party"
         destinationPrice={710}
         destinationPartyAbility={75}
         destinationAdventureAbility={40}
+      />
+      <HomeDestinationRouteItem
+        imageUrl={RouteImageTwo}
+        destinationTitle="Ultra"
+        destinationPrice={792}
+        destinationPartyAbility={90}
+        destinationAdventureAbility={25}
+      />
+      <HomeDestinationRouteItem
+        imageUrl={RouteImageThree}
+        destinationTitle="Greece"
+        destinationPrice={710}
+        destinationPartyAbility={40}
+        destinationAdventureAbility={80}
+      />
+      <HomeDestinationRouteItem
+        imageUrl={RouteImageFour}
+        destinationTitle="Adventure"
+        destinationPrice={792}
+        destinationPartyAbility={10}
+        destinationAdventureAbility={95}
       />
     </div>
   );
