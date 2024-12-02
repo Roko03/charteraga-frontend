25746fa401c3af98795ff2b1e746d23ef7948825
Roko <rokoponjarac2@gmail.com>
2024-12-02 01:20:42 +0100

    make experience card item

--- src/containers/home-page/ExperienceCardList/ExperienceCardList.tsx
+++ src/containers/home-page/ExperienceCardList/ExperienceCardList.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import styles from "./ExperienceCardList.module.scss";
+import ExperienceCardListItem from "./ExperienceCardListItem/ExperienceCardListItem";
+import SailImage from "../../../assets/images/sail.png";
+import PartyImage from "../../../assets/images/party.png";
+import ExploreImage from "../../../assets/images/explore.png";
+import RelaxImage from "../../../assets/images/relax.png";
 
 const ExperienceCardList: React.FC = () => {
-  return <div>ExperienceCardList</div>;
+  return (
+    <div>
+      <ExperienceCardListItem imageUrl={SailImage} title={"Sail"} />
+    </div>
+  );
 };
 
 export default ExperienceCardList;
