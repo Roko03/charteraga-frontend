70b1eae39cdb765a87f36230c6fd50ecbe4927eb
Roko <rokoponjarac2@gmail.com>
2024-12-02 17:49:36 +0100

    add margin to section

--- src/containers/home-page/ExperienceHighlightSection/ExperienceHighlightSection.tsx
+++ src/containers/home-page/ExperienceHighlightSection/ExperienceHighlightSection.tsx
@@ -7,14 +7,16 @@ import ExperienceHighlightList from "../ExperienceHighlightList";
 const ExperienceHighlightSection: React.FC = () => {
   return (
     <section className={styles.experience_higlight_section}>
-      <Title
-        hasTagline={false}
-        taglineVariant={"secondary"}
-        title="Come live your best life"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam."
-      />
-      <ExperienceHighlightGallery />
-      <ExperienceHighlightList />
+      <div className={styles.experience_higlight_section__container}>
+        <Title
+          hasTagline={false}
+          taglineVariant={"secondary"}
+          title="Come live your best life"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam."
+        />
+        <ExperienceHighlightGallery />
+        <ExperienceHighlightList />
+      </div>
     </section>
   );
 };
