cf6fae34ec19c90e406388990775dbfbf1a0b0e3
Roko <rokoponjarac2@gmail.com>
2024-12-02 17:37:25 +0100

    make experience highlight item

--- src/containers/home-page/ExperienceHighlightSection/ExperienceHighlightSection.tsx
+++ src/containers/home-page/ExperienceHighlightSection/ExperienceHighlightSection.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./ExperienceHighlightSection.module.scss";
 import Title from "@/components/Title";
 import ExperienceHighlightGallery from "../ExperienceHighlightGallery";
+import ExperienceHighlightItem from "../ExperienceHighlightItem";
+import Boat from "../../../assets/icons/boat.svg";
+import SurferIcon from "../../../assets/icons/surfer.svg";
+import CocktailIcon from "../../../assets/icons/cocktail.svg";
 
 const ExperienceHighlightSection: React.FC = () => {
   return (
@@ -13,6 +17,11 @@ const ExperienceHighlightSection: React.FC = () => {
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam."
       />
       <ExperienceHighlightGallery />
+      <ExperienceHighlightItem
+        highlightDescription="Spend unforgettable 7 days at sea with your crew at your own pace"
+        highlightImageUrl={Boat}
+        highlightTitle="A trip like no other"
+      />
     </section>
   );
 };
