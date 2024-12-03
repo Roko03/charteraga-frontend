e410b2dab51a150397c2ca25f1d546e37544645c
Roko <rokoponjarac2@gmail.com>
2024-12-03 10:52:33 +0100

    add testimonial

--- src/containers/destination-page/Destination/Destination.tsx
+++ src/containers/destination-page/Destination/Destination.tsx
@@ -4,6 +4,7 @@ import DestinationHighlightSection from "../DestinationHighlightSection";
 import DestinationAccordionSection from "../DestinationAccordionSection";
 import DestinationCrewSection from "../DestinationCrewSection";
 import DestinationPayAndSailSection from "../DestinationPayAndSailSection";
+import Testimonial from "@/components/Testimonial";
 
 const Destination = () => {
   return (
@@ -13,6 +14,9 @@ const Destination = () => {
       <DestinationAccordionSection />
       <DestinationCrewSection />
       <DestinationPayAndSailSection />
+      <Testimonial>
+        <h3>Ready to go?</h3>
+      </Testimonial>
     </>
   );
 };
