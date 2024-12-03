795ef5c1f15dc481a13d3c4985c44c6ef3c947e9
Roko <rokoponjarac2@gmail.com>
2024-12-03 12:33:35 +0100

    make home testomonial data

---
+++ src/containers/home-page/HomeTestimonialData/HomeTestimonialData.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./HomeTestimonialData.module.scss";
+
+const HomeTestimonialData: React.FC = () => {
+  return (
+    <div className={styles.home_testimonial_data}>
+      <h3>Our guests loved it!</h3>
+    </div>
+  );
+};
+
+export default HomeTestimonialData;
