1d4dbf12c7db995c43b64defc0fca8934d65dc02
Roko <rokoponjarac2@gmail.com>
2024-12-03 01:18:23 +0100

    make accordion item

---
+++ src/components/AccordionItem/AccordionItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./AccordionItem.module.scss";
+const AccordionItem: React.FC = () => {
+  return <div>AccordionItem</div>;
+};
+
+export default AccordionItem;
---
+++ src/containers/destination-page/DestinationAccordionList/DestinationAccordionList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./DestinationAccordionList.module.scss";
+
+const DestinationAccordionList: React.FC = () => {
+  return <div>DestinationAccordionList</div>;
+};
+
+export default DestinationAccordionList;
