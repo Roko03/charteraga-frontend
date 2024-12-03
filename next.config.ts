bc2db099b75db35fde250fde55cdcfaf1165fd37
Roko <rokoponjarac2@gmail.com>
2024-12-03 12:15:10 +0100

    remove and add dependency

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    silenceDeprecations: ["legacy-js-api"],
   },
   images: {
     remotePatterns: [
