555201c03b8f7359b7d2a831418b27cf65e0e313
Roko <rokoponjarac2@gmail.com>
2024-12-03 12:35:54 +0100

    make slider component

---
+++ src/containers/home-page/HomeReviewsSlider/HomeReviewsSlider.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HomeReviewsSlider.module.scss";
+
+const HomeReviewsSlider: React.FC = () => {
+  return <div>HomeReviewsSlider</div>;
+};
+
+export default HomeReviewsSlider;
--- src/containers/home-page/HomeTestimonialData/HomeTestimonialData.tsx
+++ src/containers/home-page/HomeTestimonialData/HomeTestimonialData.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./HomeTestimonialData.module.scss";
+import HomeReviewsSlider from "../HomeReviewsSlider";
 
 const HomeTestimonialData: React.FC = () => {
   return (
     <div className={styles.home_testimonial_data}>
       <h3>Our guests loved it!</h3>
+      <HomeReviewsSlider />
     </div>
   );
 };
