d1d76ddbb08586b18456be3a8a560a89482faaf4
Roko <rokoponjarac2@gmail.com>
2024-12-03 13:35:59 +0100

    style slider

---
+++ src/containers/home-page/HomeReviewsSlider/HomeReviewsSlide/HomeReviewsSlide.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HomeReviewsSlide.module.scss";
+
+const HomeReviewsSlide: React.FC = () => {
+  return <div className={styles.home_review_slide}></div>;
+};
+
+export default HomeReviewsSlide;
--- src/containers/home-page/HomeReviewsSlider/HomeReviewsSlider.tsx
+++ src/containers/home-page/HomeReviewsSlider/HomeReviewsSlider.tsx
@@ -1,8 +1,32 @@
+"use client";
 import React from "react";
 import styles from "./HomeReviewsSlider.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import HomeReviewsSlide from "./HomeReviewsSlide/HomeReviewsSlide";
+import "./slider.scss";
 
 const HomeReviewsSlider: React.FC = () => {
-  return <div>HomeReviewsSlider</div>;
+  return (
+    <Swiper
+      slidesPerView={"auto"}
+      spaceBetween={27}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className={styles.home_reviews_slider}
+    >
+      <SwiperSlide>
+        <HomeReviewsSlide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomeReviewsSlide />
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default HomeReviewsSlider;
