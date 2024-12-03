e77c756fedec886323e7c27501c7131acf49d0a6
Roko <rokoponjarac2@gmail.com>
2024-12-03 01:03:30 +0100

    add style to list

--- src/containers/destination-page/DestinationHighlightsList/DestinationHighlightsItem/DestinationHighlightsItem.tsx
+++ src/containers/destination-page/DestinationHighlightsList/DestinationHighlightsItem/DestinationHighlightsItem.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./DestinationHighlightsItem.module.scss";
+import Image, { StaticImageData } from "next/image";
 
-const DestinationHighlightsItem: React.FC = () => {
-  return <div>DestinationHighlightsItem</div>;
+interface DestinationHighlightsItemProps {
+  imageUrl: StaticImageData;
+  title: string;
+  description: string;
+}
+
+const DestinationHighlightsItem: React.FC<DestinationHighlightsItemProps> = ({
+  title,
+  imageUrl,
+  description,
+}) => {
+  return (
+    <div className={styles.highlights_item}>
+      <Image
+        src={imageUrl}
+        alt="image"
+        className={styles.highlights_item__image}
+      />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 export default DestinationHighlightsItem;
--- src/containers/destination-page/DestinationHighlightsList/DestinationHighlightsList.tsx
+++ src/containers/destination-page/DestinationHighlightsList/DestinationHighlightsList.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import styles from "./DestinationHighlightsList.module.scss";
+import DestinationHighlightsItem from "./DestinationHighlightsItem";
+import HighlightOne from "../../../assets/images/route-image-0.png";
 
 const DestinationHighlightsList: React.FC = () => {
-  return <div>DestinationHighlightsList</div>;
+  return (
+    <div className={styles.highlights_list}>
+      <DestinationHighlightsItem
+        imageUrl={HighlightOne}
+        title="Fjord Like Bays"
+        description="Where the sea meets the mountains. Sail the Bay of Kotor, often called Europe’s most southernmost fjord. Towering peaks and a rocky coastline dotted with charming waterside towns will leave you breathless."
+      />
+      <DestinationHighlightsItem
+        imageUrl={HighlightOne}
+        title="Fjord Like Bays"
+        description="Where the sea meets the mountains. Sail the Bay of Kotor, often called Europe’s most southernmost fjord. Towering peaks and a rocky coastline dotted with charming waterside towns will leave you breathless."
+      />
+      <DestinationHighlightsItem
+        imageUrl={HighlightOne}
+        title="Fjord Like Bays"
+        description="Where the sea meets the mountains. Sail the Bay of Kotor, often called Europe’s most southernmost fjord. Towering peaks and a rocky coastline dotted with charming waterside towns will leave you breathless."
+      />
+      <DestinationHighlightsItem
+        imageUrl={HighlightOne}
+        title="Fjord Like Bays"
+        description="Where the sea meets the mountains. Sail the Bay of Kotor, often called Europe’s most southernmost fjord. Towering peaks and a rocky coastline dotted with charming waterside towns will leave you breathless."
+      />
+    </div>
+  );
 };
 
 export default DestinationHighlightsList;
